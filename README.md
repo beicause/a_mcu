edce86722a6c49211b55fca0d992548de2684dc0
beicause <1494181792@qq.com>
2024-06-03 17:24:02 +0800

    rm mtheme

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Godot Material Color Utils
-A Godot c++ module for Material Color Utils including HCT, color scheme utils and material theme.
+A Godot c++ module for Material Color Utils including HCT and color scheme utils.
--- register_types.cpp
+++ register_types.cpp
@@ -1,6 +1,5 @@
 #include "register_types.h"
 #include "mcu.h"
-#include "mtheme.h"
 
 void initialize_a_mcu_module(ModuleInitializationLevel p_level) {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
@@ -8,7 +7,6 @@ void initialize_a_mcu_module(ModuleInitializationLevel p_level) {
 	}
 	ClassDB::register_class<Mcu>();
 	ClassDB::register_class<Hct>();
-	ClassDB::register_class<MTheme>();
 }
 
 void uninitialize_a_mcu_module(ModuleInitializationLevel p_level) {
